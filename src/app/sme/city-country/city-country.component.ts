f646f0e140a9fca5a4be0c3c0bc219bb193ec13a
mani171218 <mani.kandan@reddway.in>
2021-10-17 18:55:42 +0530

    Component added

---
+++ src/app/sme/city-country/city-country.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-city-country',
+  templateUrl: './city-country.component.html',
+  styleUrls: ['./city-country.component.css']
+})
+export class CityCountryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/sme/ultimate-beneficiary-owner/ultimate-beneficiary-owner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ultimate-beneficiary-owner',
+  templateUrl: './ultimate-beneficiary-owner.component.html',
+  styleUrls: ['./ultimate-beneficiary-owner.component.css']
+})
+export class UltimateBeneficiaryOwnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
