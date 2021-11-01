4c33c77d3e8df15e12f933e0ed7fc2138ba24bd9
manimac <manimaccse@gmail.com>
2021-11-01 10:03:41 +0530

    bug fix

--- src/app/sme/sme2/sme2.component.ts
+++ src/app/sme/sme2/sme2.component.ts
@@ -17,11 +17,28 @@ export class Sme2Component implements OnInit {
   showSummaryForm: boolean = false;
   currentPan: any = '';
   currentForm: any = '';
+  @ViewChild('pathFundDetails', { static: false }) pathFundDetails:any;
+  @ViewChild('pathIdentifyYourself', { static: false }) pathIdentifyYourself:any;
+  @ViewChild('pathInvestorDetails', { static: false }) pathInvestorDetails:any;
+  @ViewChild('pathInvestmentDetails', { static: false }) pathInvestmentDetails:any;
+  @ViewChild('pathPayment', { static: false }) pathPayment:any;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    this.setProgressBar(this.pathFundDetails.nativeElement, 100);
+    this.setProgressBar(this.pathIdentifyYourself.nativeElement, 75);
+    this.setProgressBar(this.pathInvestorDetails.nativeElement, 50);
+    this.setProgressBar(this.pathInvestmentDetails.nativeElement, 25);
+    this.setProgressBar(this.pathPayment.nativeElement, 0);
+  }
+
+  setProgressBar(elem:any, percent:any) {
+    elem.style.strokeDashoffset =  elem.getTotalLength() * ((100 - percent) /100);
+  }
+  
   showFund() {
     this.showFundForm = true;
     this.showPanForm = false;
