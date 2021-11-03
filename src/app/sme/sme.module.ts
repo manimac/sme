92eebeeb4d3b6e54e8d0e5fc0d8b94b9f85ab33d
manimac <manimaccse@gmail.com>
2021-11-03 22:54:00 +0530

    removed unnecessary component

--- src/app/sme/sme.module.ts
+++ src/app/sme/sme.module.ts
@@ -5,14 +5,7 @@ import { SmeRoutingModule } from './sme-routing.module';
 import { PanFormComponent } from './pan-form/pan-form.component';
 import { KycFormComponent } from './kyc-form/kyc-form.component';
 import { SmeComponent } from './sme.component';
-import { CityCountryComponent } from './city-country/city-country.component';
 import { AdditionalDetailsComponent } from './additional-details/additional-details.component';
-import { FactaCrsDetailsComponent } from './facta-crs-details/facta-crs-details.component';
-import { PartAComponent } from './part-a/part-a.component';
-import { PartBComponent } from './part-b/part-b.component';
-import { UltimateBeneficiaryOwnerComponent } from './ultimate-beneficiary-owner/ultimate-beneficiary-owner.component';
-import { BankDetailsComponent } from './bank-details/bank-details.component';
-import { UploadDocumentsComponent } from './upload-documents/upload-documents.component';
 import { SummaryComponent } from './summary/summary.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DatepickerModule, BsDatepickerModule } from 'ngx-bootstrap/datepicker';
@@ -29,7 +22,7 @@ import { FundComponent } from './sme2/fund/fund.component';
 import { VerifiedPanFormComponent } from './sme2/verified-pan-form/verified-pan-form.component';
 registerLocaleData(localeDECH);
 @NgModule({
-  declarations: [PanFormComponent, KycFormComponent, SmeComponent, CityCountryComponent, AdditionalDetailsComponent, FactaCrsDetailsComponent, PartAComponent, PartBComponent, UltimateBeneficiaryOwnerComponent, BankDetailsComponent, UploadDocumentsComponent, SummaryComponent, AcceptOnlyNumberDirective, FilterArrayPipe, Sme2Component, VerifiedKycComponent, IncorporationComponent, FundComponent, VerifiedPanFormComponent],
+  declarations: [PanFormComponent, KycFormComponent, SmeComponent, AdditionalDetailsComponent, SummaryComponent, AcceptOnlyNumberDirective, FilterArrayPipe, Sme2Component, VerifiedKycComponent, IncorporationComponent, FundComponent, VerifiedPanFormComponent],
   imports: [
     CommonModule,
     SmeRoutingModule,
